56db013158dfe6b0f2a9a71e8218428bf8f43fa7
huntermelrose <huntermelrose831@gmail.com>
2025-06-15 16:17:44 -0700

    almost donegit add --allgit add --all

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import "./Header.css";
 import logo from "../../assets/wtwrlogo.svg";
 import avatar from "../../assets/avatar.png";
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <>
       <header className="header">
         <img className="header__logo" src={logo} />
         <p className="header__date-location">DATE, LOCATION</p>
-        <button className="header__add-clothes-btn">Add clothes</button>
+        <button
+          type="button"
+          onClick={handleAddClick}
+          className="header__add-clothes-btn"
+        >
+          Add clothes
+        </button>
 
         <div className="header__user-container">
           <p className="header__username">Terrence Tegegne</p>
--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import WeatherCard from "../WeatherCard/WeatherCard.jsx";
 import { defaultClothingItems } from "../../utils/constants.js";
 import ItemCard from "../ItemCard/ItemCard.jsx";
 import "../Main/Main.css";
-function Main({ weatherData }) {
+function Main({ weatherData, handleCardClick }) {
   return (
     <main>
       <WeatherCard />
@@ -16,7 +16,13 @@ function Main({ weatherData }) {
               return item.weather === weatherData.type;
             })
             .map((item) => {
-              return <ItemCard key={item._id} item={item} />;
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={handleCardClick}
+                />
+              );
             })}
         </ul>
       </section>
